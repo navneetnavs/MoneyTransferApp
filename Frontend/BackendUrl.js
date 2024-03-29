5a81df761eb96a130d4324b4c8f1c01f2ab81b5c
navneetnavs <navneetkumar2004@icloud.com>
2024-03-30 03:56:22 +0530

    updated

--- Frontend/BackendUrl.js
+++ Frontend/BackendUrl.js
@@ -1,3 +1,3 @@
-const Backend_Url = "https://moneytransferapp.onrender.com"
+const Backend_Url = "https://moneytransferapp.onrender.com/api/v1"
 // const Backend_Url = ""
 export default Backend_Url;
